0134bdecb09bb5ebe19ed113a95efc2fa9bf333a
Erickuokuo <eric_kuo@outlook.com>
2024-04-24 23:04:16 -0700

    finished

---
+++ lifecounterV2/lifecounterV2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lifecounterV2
+//
+//  Created by Eric Kuo on 4/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
